jcfr/iMSTK

commit f2a60e16bc5e6a7cd557251e147a9c6327337d6e

COMP: Rename external project from "openvr" to "OpenVR"
This commit renames the project so that its name is consistent with
VTK established convention providing the vtkRenderingOpenVR module.

CMake/FindOpenVR.cmake
@@ -1,0 +1,11 @@
+include(imstkFind)
+#-----------------------------------------------------------------------------
+# Find All Headers and Libraries for openvr
+#-----------------------------------------------------------------------------
+
+imstk_find_header(OpenVR openvr.h openvr)
+imstk_find_libary(OpenVR openvr_api " ")#Use same library for debug
+imstk_find_package(OpenVR OpenVR::OpenVR)
+
+#message(STATUS "OpenVR include : ${OpenVR_INCLUDE_DIRS}")
+#message(STATUS "OpenVR libraries : ${OpenVR_LIBRARIES}")

CMake/External/External_OpenVR.cmake
@@ -32,7 +32,7 @@ endif()
 set(copy_openvr_headers_command
   ${CMAKE_COMMAND} -E copy_directory
   ${OPENVR_EXTRACT_DIR}/headers
-  ${CMAKE_INSTALL_PREFIX}/include/openvr
+  ${CMAKE_INSTALL_PREFIX}/include/OpenVR
   )
 set(copy_openvr_dll_command
   ${CMAKE_COMMAND} -E copy
@@ -49,7 +49,7 @@ set(copy_openvr_lib_command
 # Add External Project
 #-----------------------------------------------------------------------------
 include(imstkAddExternalProject)
-imstk_add_external_project( openvr
+imstk_add_external_project( OpenVR
   URL "https://gitlab.kitware.com/iMSTK/openvr/-/archive/v1.12.5/openvr-v1.12.5.tar.gz"
   URL_MD5 "42c4d1c4a194860aaa4e793b009b99c0"
   DOWNLOAD_DIR ${OPENVR_PREFIX}

CMake/External/External_VTK.cmake
@@ -1,7 +1,7 @@
 #-----------------------------------------------------------------------------
 # Dependencies
 #-----------------------------------------------------------------------------
-set(VTK_DEPENDENCIES "openvr")
+set(VTK_DEPENDENCIES "OpenVR")
 
 set(${PROJECT_NAME}_VTK_REPO_SOURCE "9.0" CACHE STRING "Select VTK Source Branch/Tag")
 set(VTK_SOURCES "9.0;master;release;nightly-master" CACHE INTERNAL "List of available VTK branch,tags to get")

CMake/Findopenvr.cmake
@@ -1,11 +1,0 @@
-include(imstkFind)
-#-----------------------------------------------------------------------------
-# Find All Headers and Libraries for openvr
-#-----------------------------------------------------------------------------
-
-imstk_find_header(openvr openvr.h openvr)
-imstk_find_libary(openvr openvr_api " ")#Use same library for debug
-imstk_find_package(openvr openvr::openvr)
-
-#message(STATUS "openvr include : ${openvr_INCLUDE_DIRS}")
-#message(STATUS "openvr libraries : ${openvr_LIBRARIES}")

CMake/iMSTKConfig.cmake.in
@@ -51,7 +51,7 @@ find_package( LibNiFalcon REQUIRED)
 find_package( Libusb REQUIRED)
 
 # OpenVR
-find_package( openvr REQUIRED )
+find_package( OpenVR REQUIRED )
 
 # PhysX
 if (iMSTK_USE_PHYSX)
@@ -86,9 +86,7 @@ if(iMSTK_USE_OpenHaptics)
 endif()
 
 # VTK
-list(GET openvr_INCLUDE_DIRS 0 OpenVR_INCLUDE_DIR)
-list(GET openvr_LIBRARIES 1 OpenVR_LIBRARY)
-set(OpenVR_INCLUDE_DIR ${OpenVR_INCLUDE_DIR} CACHE INTERNAL "")
+list(GET OpenVR_LIBRARIES 1 OpenVR_LIBRARY)
 set(OpenVR_LIBRARY ${OpenVR_LIBRARY} CACHE INTERNAL "")
 set(VTK_DIR "@VTK_DIR@")
 find_dependency(VTK CONFIG)

CMakeLists.txt
@@ -165,7 +165,7 @@ if(${PROJECT_NAME}_SUPERBUILD)
   imstk_define_dependency(Eigen3)
   imstk_define_dependency(g3log)
   imstk_define_dependency(LibNiFalcon)
-  imstk_define_dependency(openvr)
+  imstk_define_dependency(OpenVR)
   if (${PROJECT_NAME}_USE_PHYSX)
     imstk_define_dependency(PhysX)
   endif()
@@ -249,7 +249,7 @@ find_package( LibNiFalcon REQUIRED)
 # Libusb
 find_package( Libusb REQUIRED)
 # OpenVR
-find_package( openvr REQUIRED )
+find_package( OpenVR REQUIRED )
 
 # Select the release version of PhysX to use
 if (${PROJECT_NAME}_USE_PHYSX)

Source/Rendering/CMakeLists.txt
@@ -49,7 +49,7 @@ set(RENDERING_SUBDIR
   VTKRenderer/RenderDelegate)
 set(RENDERING_DEPENDENCIES  
   Materials
-  openvr::openvr)
+  OpenVR::OpenVR)
 
 include(imstkAddLibrary)
 imstk_add_library( Rendering

commit b0d7195c67151c1bb25ad703050f2e5f34384b6c

COMP: Update FindOpenVR to fix configuration on case-sensitive system
This commit fixes a regression introduced in f2a60e16b (COMP: Rename
external project from "openvr" to "OpenVR")

CMake/FindOpenVR.cmake
@@ -5,7 +5,7 @@ include(imstkFind)
 
 set(_sub_dir "")
 if(NOT DEFINED OpenVR_ROOT_DIR)
-  set(_sub_dir "openvr")
+  set(_sub_dir "OpenVR")
 endif()
 
 imstk_find_header(OpenVR openvr.h ${_sub_dir})