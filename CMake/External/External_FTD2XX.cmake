235c929fafc15f5df9daa9548ca1e2840614f481
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-11-12 19:01:24 -0500

    COMP: Update FTD2XX external project to support reuse from application
    Introduce variables FTD2XX_DLL_DIR, FTD2XX_DLL_NAME, FTD2XX_LIB_DIR
    and FTD2XX_LIB_NAME for use in application implementing their own
    external  project including the ones provided by iMSTK.

--- CMake/External/External_FTD2XX.cmake
+++ CMake/External/External_FTD2XX.cmake
@@ -9,41 +9,67 @@ include(imstkAddExternalProject)
 # Set FTD2XX_SOURCE_DIR
 imstk_define_external_dirs( FTD2XX )
 
+# Directories and filenames specific to the OpenVR archive layout
+set(FTD2XX_DLL_DIR "i386")
+set(FTD2XX_DLL_NAME "ftd2xx.dll")
+set(FTD2XX_LIB_DIR "i386")
+set(FTD2XX_LIB_NAME "ftd2xx.lib")
+if("${CMAKE_SIZEOF_VOID_P}" STREQUAL "8")
+  set(FTD2XX_DLL_DIR "amd64")
+  set(FTD2XX_DLL_NAME "ftd2xx64.dll")
+  set(FTD2XX_LIB_DIR "amd64")
+  set(FTD2XX_LIB_NAME "ftd2xx.lib")
+endif()
+
 #-----------------------------------------------------------------------------
 # Set install commands
 #-----------------------------------------------------------------------------
-set(FTD2XX_INSTALL_DIR ${CMAKE_INSTALL_PREFIX})
+if(CMAKE_PROJECT_NAME STREQUAL "iMSTK")
+  set(FTD2XX_INSTALL_DIR ${CMAKE_INSTALL_PREFIX})
 
-set(ftd2xx_libdir "i386")
-if("${CMAKE_SIZEOF_VOID_P}" STREQUAL "8")
-  set(ftd2xx_libdir "amd64")
+  set(copy_ftd2xx_headers_command
+    ${CMAKE_COMMAND} -E copy
+    ${FTD2XX_SOURCE_DIR}/ftd2xx.h
+    ${FTD2XX_INSTALL_DIR}/include/ftd2xx/ftd2xx.h
+    )
+  set(copy_ftd2xx_lib_command
+    ${CMAKE_COMMAND} -E copy
+    ${FTD2XX_SOURCE_DIR}/${FTD2XX_LIB_DIR}/${FTD2XX_LIB_NAME}
+    ${FTD2XX_INSTALL_DIR}/lib/${FTD2XX_LIB_NAME}
+    )
+  set(copy_ftd2xx_dll_command
+    ${CMAKE_COMMAND} -E copy
+    ${FTD2XX_SOURCE_DIR}/${FTD2XX_DLL_DIR}/${FTD2XX_DLL_NAME}
+    ${FTD2XX_INSTALL_DIR}/bin/${FTD2XX_DLL_NAME}
+    )
+
+  set(FTD2XX_INSTALL_COMMAND
+    INSTALL_COMMAND
+      COMMAND ${copy_ftd2xx_headers_command}
+      COMMAND ${copy_ftd2xx_lib_command}
+      COMMAND ${copy_ftd2xx_dll_command}
+    )
+
+  if("${CMAKE_SIZEOF_VOID_P}" STREQUAL "8")
+    # HS 2021-aug-08 Some dependencies expect the dll name to
+    # be ftd2xx.dll rather than ftd2xx64.dll we're copying here
+    # to the renamed version as the whole chain is built via 64 bit builds
+    set(copy_ftd2xx_dll_command_rename
+      ${CMAKE_COMMAND} -E copy
+      ${FTD2XX_SOURCE_DIR}/${FTD2XX_LIB_DIR}/ftd2xx64.dll
+      ${FTD2XX_INSTALL_DIR}/bin/ftd2xx.dll
+      )
+
+    list(APPEND FTD2XX_INSTALL_COMMAND
+      COMMAND ${copy_ftd2xx_dll_command_rename}
+      )
+  endif()
+else()
+  set(FTD2XX_INSTALL_COMMAND
+    INSTALL_COMMAND
+      COMMAND ${SKIP_STEP_COMMAND}
+    )
 endif()
-
-set(copy_ftd2xx_headers_command
-  ${CMAKE_COMMAND} -E copy
-  ${FTD2XX_SOURCE_DIR}/ftd2xx.h
-  ${FTD2XX_INSTALL_DIR}/include/ftd2xx/ftd2xx.h
-  )
-set(copy_ftd2xx_lib_command
-  ${CMAKE_COMMAND} -E copy
-  ${FTD2XX_SOURCE_DIR}/${ftd2xx_libdir}/ftd2xx.lib
-  ${FTD2XX_INSTALL_DIR}/lib/ftd2xx.lib
-  )
-  
-set(copy_ftd2xx_dll_command
-  ${CMAKE_COMMAND} -E copy
-  ${FTD2XX_SOURCE_DIR}/${ftd2xx_libdir}/ftd2xx64.dll
-  ${FTD2XX_INSTALL_DIR}/bin/ftd2xx64.dll
-  )
-
-# HS 2021-aug-08 Some dependencies expect the dll name to 
-# be ftd2xx.dll rather than ftd2xx64.dll we're copying here
-# to the renamed version as the whole chain is built via 64 bit builds
-set(copy_ftd2xx_dll_command_rename
-  ${CMAKE_COMMAND} -E copy
-  ${FTD2XX_SOURCE_DIR}/${ftd2xx_libdir}/ftd2xx64.dll
-  ${FTD2XX_INSTALL_DIR}/bin/ftd2xx.dll
-  )
 
 
 #-----------------------------------------------------------------------------
@@ -57,11 +83,7 @@ imstk_add_external_project( FTD2XX
   UPDATE_COMMAND ${SKIP_STEP_COMMAND}
   CONFIGURE_COMMAND ${SKIP_STEP_COMMAND}
   BUILD_COMMAND ${SKIP_STEP_COMMAND}
-  INSTALL_COMMAND
-    COMMAND ${copy_ftd2xx_headers_command}
-    COMMAND ${copy_ftd2xx_lib_command}
-    COMMAND ${copy_ftd2xx_dll_command}
-    COMMAND ${copy_ftd2xx_dll_command_rename}
+  ${FTD2XX_INSTALL_COMMAND}
   RELATIVE_INCLUDE_PATH "/"
   #VERBOSE
 )
