ab98b287ec351b61b148e7c2443fa32cbc3f7851
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-11-15 09:44:22 -0500

    COMP: Update Libusb external project to support reuse from application
    Introduce variables Libusb_DLL_DIR, Libusb_DLL_NAME, Libusb_LIB_DIR
    and Libusb_LIB_NAME for use in application implementing their own
    external  project including the ones provided by iMSTK.

--- CMake/External/External_Libusb.cmake
+++ CMake/External/External_Libusb.cmake
@@ -9,31 +9,56 @@ include(imstkAddExternalProject)
 # Set Libusb_SOURCE_DIR and Libusb_PREFIX
 imstk_define_external_dirs( Libusb )
 
+# Directories and filenames specific to the Libusb archive layout
+set(Libusb_INC_DIR "include/libusb-1.0")
+set(_libdir "MS32")
+if("${CMAKE_SIZEOF_VOID_P}" STREQUAL "8")
+  set(_libdir "MS64")
+endif()
+set(Libusb_DLL_DIR "${_libdir}")
+set(Libusb_DLL_NAME "libusb-1.0.dll")
+set(Libusb_LIB_DIR "${_libdir}")
+set(Libusb_LIB_NAME "libusb-1.0.lib")
+
 #-----------------------------------------------------------------------------
 # Set install commands
 #-----------------------------------------------------------------------------
-set(Libusb_INSTALL_DIR ${CMAKE_INSTALL_PREFIX})
+if(CMAKE_PROJECT_NAME STREQUAL "iMSTK")
+  set(Libusb_INSTALL_DIR ${CMAKE_INSTALL_PREFIX})
 
-set(libusb_libdir "MS32")
-if("${CMAKE_SIZEOF_VOID_P}" STREQUAL "8")
-  set(libusb_libdir "MS64")
+  set(libusb_libdir "MS32")
+  if("${CMAKE_SIZEOF_VOID_P}" STREQUAL "8")
+    set(libusb_libdir "MS64")
+  endif()
+
+  set(copy_libusb_headers_command
+    ${CMAKE_COMMAND} -E copy_directory
+    ${Libusb_SOURCE_DIR}/include
+    ${Libusb_INSTALL_DIR}/include
+    )
+  set(copy_libusb_lib_command
+    ${CMAKE_COMMAND} -E copy
+    ${Libusb_SOURCE_DIR}/${libusb_libdir}/dll/libusb-1.0.lib
+    ${Libusb_INSTALL_DIR}/lib/libusb-1.0.lib
+    )
+  set(copy_libusb_dll_command
+    ${CMAKE_COMMAND} -E copy
+    ${Libusb_SOURCE_DIR}/${libusb_libdir}/dll/libusb-1.0.dll
+    ${Libusb_INSTALL_DIR}/bin
+    )
+
+  set(Libusb_INSTALL_COMMAND
+    INSTALL_COMMAND
+      COMMAND ${copy_libusb_headers_command}
+      COMMAND ${copy_libusb_lib_command}
+      COMMAND ${copy_libusb_dll_command}
+    )
+else()
+  set(Libusb_INSTALL_COMMAND
+    INSTALL_COMMAND
+      COMMAND ${SKIP_STEP_COMMAND}
+    )
 endif()
-
-set(copy_libusb_headers_command
-  ${CMAKE_COMMAND} -E copy_directory
-  ${Libusb_SOURCE_DIR}/include
-  ${Libusb_INSTALL_DIR}/include
-  )
-set(copy_libusb_lib_command
-  ${CMAKE_COMMAND} -E copy
-  ${Libusb_SOURCE_DIR}/${libusb_libdir}/dll/libusb-1.0.lib
-  ${Libusb_INSTALL_DIR}/lib/libusb-1.0.lib
-  )
-set(copy_libusb_dll_command
-  ${CMAKE_COMMAND} -E copy
-  ${Libusb_SOURCE_DIR}/${libusb_libdir}/dll/libusb-1.0.dll
-  ${Libusb_INSTALL_DIR}/bin
-  )
 
 #-----------------------------------------------------------------------------
 # Add External Project
@@ -46,10 +71,7 @@ imstk_add_external_project( Libusb
   UPDATE_COMMAND ${SKIP_STEP_COMMAND}
   CONFIGURE_COMMAND ${SKIP_STEP_COMMAND}
   BUILD_COMMAND ${SKIP_STEP_COMMAND}
-  INSTALL_COMMAND
-    COMMAND ${copy_libusb_headers_command}
-    COMMAND ${copy_libusb_lib_command}
-    COMMAND ${copy_libusb_dll_command}
-  RELATIVE_INCLUDE_PATH "include/libusb-1.0"
+  ${Libusb_INSTALL_COMMAND}
+  RELATIVE_INCLUDE_PATH "${Libusb_INC_DIR}"
   #VERBOSE
 )
