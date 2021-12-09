4bdcf5e96c0ac97340ae14c0b0fd0ca971100355
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-12-08 23:17:31 -0500

    COMP: Fix iMSTK integration updating VegaFEM and removing obsolete PThreads project
    Considering that in VegaFEM v4.0, pthreads was replaced with
    Intel TBB (see [1]), this commit removes the references to "pthread" and
    also removes the corresponding custom CMake module.
    
    [1] http://barbic.usc.edu/vega/download.html
    
    It also fixes error like the following reported when integrating iMSTK in
    project (e.g Slicer-based app) already looking up threading libraries
    using "FindThreads" module provided by CMake:
    
       -- Found PThreads: C:/path/to/SlicerCustomApp-build/PThreads-install/include/pthread
        CMake Error at C:/path/to/SlicerCustomApp-build/iMSTK/CMake/Utilities/imstkFind.cmake:214 (add_library):
          add_library cannot create imported target "Threads::Threads" because
          another target with the same name already exists.
        Call Stack (most recent call first):
          C:/path/to/SlicerCustomApp-build/iMSTK/CMake/FindPThreads.cmake:8 (imstk_find_package)
          C:/path/to/SlicerCustomApp-build/VegaFEM-install/lib/cmake/VegaFEM/VegaFEMConfig.cmake:8 (find_package)
          C:/path/to/SlicerCustomApp-build/CTK/CMake/CMakeFindDependencyMacro.cmake:90 (find_package)
          C:/path/to/SlicerCustomApp-build/imstk-build/iMSTKConfig.cmake:144 (find_dependency)
          C:/path/to/SlicerCustomApp/Modules/CLI/MyCLI/CMakeLists.txt:33 (find_package)
    
    List of VegaFEM changes:
    
    $ git shortlog 32b7669..d67de54 --no-merges
    Jean-Christophe Fillion-Robin (1):
          COMP: Remove obsolete and unused PThreads requirement

--- CMake/External/External_VegaFEM.cmake
+++ CMake/External/External_VegaFEM.cmake
@@ -9,9 +9,6 @@ endif()
 # Add External Project
 #-----------------------------------------------------------------------------
 set(VegaFEM_DEPENDENCIES "")
-if(WIN32)
-  list(APPEND VegaFEM_DEPENDENCIES PThreads)
-endif(WIN32)
 
 set(EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS)
 # After updating the CMake minimum required version to >= 3.15, package
@@ -29,12 +26,12 @@ endif()
 
 # Download options
 if(NOT DEFINED iMSTK_VegaFEM_GIT_SHA)
-  set(iMSTK_VegaFEM_GIT_SHA "32b766921e9e4ff5284b7a596ad8d4c58470c5d3") # build_model_reduction
+  set(iMSTK_VegaFEM_GIT_SHA "d67de5452a472d3aa1b8902e1bf60e8f9834c664") # build_model_reduction
 endif()
 if(NOT DEFINED iMSTK_VegaFEM_GIT_REPOSITORY)
   set(EXTERNAL_PROJECT_DOWNLOAD_OPTIONS
     URL https://gitlab.kitware.com/jcfr/vegafemv4.0/-/archive/${iMSTK_VegaFEM_GIT_SHA}/vegafemv4.0-${iMSTK_VegaFEM_GIT_SHA}.zip
-    URL_HASH MD5=80984e88a272ee981b11402231e9c4bf
+    URL_HASH MD5=0bb27fce7e31a99ed38b91a0bf1b2203
     )
 else()
   set(EXTERNAL_PROJECT_DOWNLOAD_OPTIONS
@@ -46,7 +43,6 @@ endif()
 imstk_add_external_project( VegaFEM
   ${EXTERNAL_PROJECT_DOWNLOAD_OPTIONS}
   CMAKE_CACHE_ARGS
-    -DVegaFEM_ENABLE_PTHREADS_SUPPORT:BOOL=ON
     -DVegaFEM_ENABLE_OpenGL_SUPPORT:BOOL=OFF
     -DVegaFEM_BUILD_MODEL_REDUCTION:BOOL=${${PROJECT_NAME}_USE_MODEL_REDUCTION}
     -DVegaFEM_BUILD_UTILITIES:BOOL=OFF
